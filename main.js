50bd05f88ae0f6f0317798c57f3c2a181651292b
razannael <nailrazan40@gmail.com>
2024-05-28 23:21:47 +0300

    Parallax Effect Part6

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ let xVal = 0,
         xVal = e.clientX - window.innerWidth / 2;
         yVal = e.clientY - window.innerHeight / 2;
 
+    
         parallax_el.forEach((el)=>{ 
             let speedx = el.dataset.speedx;
             let speedy = el.dataset.speedy;
-            el.style.transform = `translateX(calc(-50% + ${-xVal * speedx}px)) translateY(calc(-50% + ${yVal * speedy}px))`
+            
+            let isLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+            let zVal = (e.clientX - parseFloat(getComputedStyle(el).left)) * isLeft * 0.09;
+            el.style.transform = `translateX(calc(-50% + ${-xVal * speedx}px)) translateY(calc(-50% + ${yVal * speedy}px)) perspective(2300px) translateZ(${zVal}px)`;
     });
 
     });
