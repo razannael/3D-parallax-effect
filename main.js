0dd91b149fee00edc9e86dfa8212f38225b5884a
razannael <nailrazan40@gmail.com>
2024-05-29 00:06:40 +0300

    Parallax Effect Done

--- main.js
+++ main.js
@@ -2,19 +2,29 @@ const parallax_el = document.querySelectorAll(".parallax");
 let xVal = 0,
     yVal = 0;
 
-    window.addEventListener('mousemove', (e)=>{
-        xVal = e.clientX - window.innerWidth / 2;
-        yVal = e.clientY - window.innerHeight / 2;
+    let rotateDegree = 0;
 
-    
+
+    const update = (cursorPosition) => {
         parallax_el.forEach((el)=>{ 
             let speedx = el.dataset.speedx;
             let speedy = el.dataset.speedy;
             let speedz = el.dataset.speedz;
+            let rotateSpeed = el.dataset.rotation;
 
             let isLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
-            let zVal = (e.clientX - parseFloat(getComputedStyle(el).left)) * isLeft * 0.09;
-            el.style.transform = `translateX(calc(-50% + ${-xVal * speedx}px)) translateY(calc(-50% + ${yVal * speedy}px)) perspective(2300px) translateZ(${zVal * speedz}px)`;
+            let zVal = (cursorPosition - parseFloat(getComputedStyle(el).left)) * isLeft * 0.09;
+            el.style.transform = `translateX(calc(-50% + ${-xVal * speedx}px)) rotateY(${rotateDegree * rotateSpeed}deg) translateY(calc(-50% + ${yVal * speedy}px)) perspective(2300px) translateZ(${zVal * speedz}px)`;
     });
+    }
+
+    update(0);
+    window.addEventListener('mousemove', (e)=>{
+        xVal = e.clientX - window.innerWidth / 2;
+        yVal = e.clientY - window.innerHeight / 2;
+        
+        rotateDegree = xVal / (window.innerWidth / 2) * 20;
+
+        update(e.clientX);
 
     });
